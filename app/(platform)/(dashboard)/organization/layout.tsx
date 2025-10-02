f8be9bd32cf12077b1514081b138e32bd949afe2
oleksiiyovenko <aleksey.xmen@gmail.com>
2025-10-02 22:42:36 +0300

    add: workspaces sidebars

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Sidebar from '../_components/sidebar';
 
 export default function OrganizationLayout({
   children,
@@ -8,7 +9,9 @@ export default function OrganizationLayout({
   return (
     <main className='pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto'>
       <div className='flex gap-x-7'>
-        <div className='w-64 shrink-0 hidden md:block'>{/*sidebar*/}</div>
+        <div className='w-64 shrink-0 hidden md:block'>
+          <Sidebar />
+        </div>
         {children}
       </div>
     </main>
